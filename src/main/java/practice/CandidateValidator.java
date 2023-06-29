fc1c7980553cebff51992327f160282799e4d186
Slava-png <pylyp.sviatoslav@gmail.com>
2023-06-29 23:29:10 +0200

    implemented logic for methods using stream-api

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final int MIN_AGE = 35;
+    private static final int MIN_TIME_IN_UKR = 10;
+    private static final int FROM_YEAR = 0;
+    private static final int TO_YEAR = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && Integer.parseInt(years[TO_YEAR])
+                - Integer.parseInt(years[FROM_YEAR]) >= MIN_TIME_IN_UKR;
+    }
 }
